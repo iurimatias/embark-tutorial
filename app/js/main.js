14bcce88131bc71efebab00bba5dd4cdb8ac59d9
Iuri Matias <iuri.matias@gmail.com>
2015-10-16 13:45:36 -0400

    add token contract; listen for transfer event

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+
+var event = token.CoinTransfer({}, '', function(error, result){
+  if (!error)
+    console.log("Coin transfer: " + result.args.amount + " tokens were sent. Balances now are as following: \n Sender:\t" + result.args.sender + " \t" + token.coinBalanceOf.call(result.args.sender) + " tokens \n Receiver:\t" + result.args.receiver + " \t" + token.coinBalanceOf.call(result.args.receiver) + " tokens" )
+});
+
+
